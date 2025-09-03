a2511c6e661b4709f7892aa033a0a87060449e75
Luis  da Silva <156721982+xKuth@users.noreply.github.com>
2025-09-02 23:02:20 -0300

    Update CartasSuperTrunfo.c
    Adicionado cálculos de densidade demográfica e Pib per capita, e seus mostradores com printf!

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
 int main(){
-    // Variaveis Inicializadas:
     char estado[2], cod_carta[2], estado2[2], cod_carta2[2], cidade[20], cidade2[20];
     int populacao, pontos_turi, populacao2, pontos_turi2;
-    float area, pib, area2, pib2;
-
+    float area, pib ,densi_demo, pib_perca ,densi_demo2 , pib_perca2, area2, pib2;
+ 
     // Inicio primeira carta
     printf("Digite um estado para carta use apenas[A a H] maiuscula: ");
     scanf("%s", &estado);
@@ -17,8 +16,16 @@ int main(){
     scanf("%d", &populacao);
     printf("Digite o tamanho do territorio em km² [use '.' em vez de ','] ");
     scanf("%f", &area);
+
+    // Calculo de Densidade demografica:
+    densi_demo = (float) populacao / area;
+
     printf("Digite o PIB (produto interno bruto) da sua cidade [user '.' em vez de ',']: ");
     scanf("%f", &pib);
+
+    // Calculo de Pib per capita da população:
+    pib_perca = (float) pib / populacao;
+
     printf("Digite quantos pontos turisticos sua cidade ira ter [apenas inteiros ex:1 a 999] ");
     scanf("%d" , &pontos_turi);
     //Fim primeira carta:
@@ -36,18 +43,32 @@ int main(){
     scanf("%d", &populacao2);
     printf("Digite o tamanho do territorio em km² [use '.' em vez de ','] ");
     scanf("%f", &area2);
+
+    // Calculo de Densidade demografica 2:
+    densi_demo2 = (float) populacao2 / area2;
+
     printf("Digite o PIB (produto interno bruto) da sua cidade [user '.' em vez de ',']: ");
     scanf("%f", &pib2);
+
+    // Calculo de Pib per capita da população 2:
+    pib_perca2 = (float) pib2 / populacao2;
+
     printf("Digite quantos pontos turisticos sua cidade ira ter [apenas inteiros ex:1 a 999] ");
     scanf("%d" , &pontos_turi2);
     //Fim segunda carta
-    // Declaração das cartas preenchidas!
+
     printf("-----------------------------------------\n");
     printf("Suas cartas foram : ↓\n");
+    // Impressão da 1º carta:
     printf("Carta 1: \n Estado: %s\n Codigo: %s0%s \n Nome da cidade: %s\n população: %d\n Area: %.2f\n PIB: %.2f\n Numero de pontos turísticos %d",
         estado, estado, cod_carta, cidade, populacao, area, pib, pontos_turi);
-    printf("-----------------------------------------\n");
-    printf("Carta 2: \n Estado: %s\n Codigo: %s0%s \n Nome da cidade: %s\n população: %d\n Area: %.2f\n PIB: %.2f\n Numero de pontos turísticos %d\n",
+    printf("\nDensidade populacional: %.2f hab/km²\n Pib per capita: %.2f reais", densi_demo, pib_perca);
+    
+    // impressão da 2º carta:
+    printf("\n-----------------------------------------\n");
+    printf("Carta 2: \n Estado: %s\n Codigo: %s0%s \n Nome da cidade: %s\n população: %d\n Area: %.2f\n PIB: %.2f\n Numero de pontos turísticos %d",
             estado2, estado2, cod_carta2, cidade2, populacao2, area2, pib2, pontos_turi2);
+    printf("\nDensidade populacional: %.2f hab/km²\n Pib per capita: %.2f reais", densi_demo2, pib_perca2);
+
     return 0;
 };
